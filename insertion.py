b68a0eee2c01ddf54badb16f3d9ffb3ba8e30907
himanshusaroj <himanshusaroj1997@gmail.com>
2023-10-25 11:23:33 +0530

    work

--- insertion.py
+++ insertion.py
@@ -3,5 +3,21 @@ my_array = arr.array('i', [1,2,4,5,6,567,9])
 my_array.insert(0,50)
 my_array.insert(4,50)
 my_array.insert(2,50)
+
 print(my_array)
  
+
+def trav(my_array):
+    for i in my_array:
+        print(i)
+
+out = trav(my_array)
+
+
+def access(my_array, index):
+    if index> len(my_array):
+        print('out of index')
+    else :
+        print(my_array[index])
+
+out2=access(my_array,3)
