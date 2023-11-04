2ae1e7610d7299b4bed5534a5a61b5c8c5c57650
himanshusaroj <himanshusaroj1997@gmail.com>
2023-11-04 19:22:00 +0530

    4N

---
+++ TwoDarray.py
@@ -0,0 +1,8 @@
+arr=[]
+rows, cols=5,5
+for i in range(rows):
+	col = []
+	for j in range(cols):
+		col.append(input(""))
+	arr.append(col)
+print(arr)
--- createarray.py
+++ createarray.py
@@ -1,4 +1,6 @@
-import array 
+import array
 
-my_array = array.array('i',[1,2,3,45,5]) 
+my_array = array.array('i',[4,6,8,3,7])
+my_array.insert(4,5)
+
 print(my_array)
